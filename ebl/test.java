0df37667df087c3b0dc3a234e07b1ab70406857f
xhchen <ahooy.chen@gmail.com>
2014-09-30 13:38:16 +0200

    The subroutine for EBL absorption

---
+++ ebl/test.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+class test{
+	public static void main(String[] args){
+double[] nu = {1e25,1e26};
+double z = 0.6035;
+double[] tau;
+tau = franceschini.tau_IRA(nu, z);
+System.out.println("tau= " + Arrays.toString(tau));
+}
+}
